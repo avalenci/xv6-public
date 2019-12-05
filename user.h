276b8d0a332efa2d83a32a694e621c7679a045c5
Valenci, Andrew <ansmeew@gmail.com>
2019-12-05 11:44:23 -0500

    Update user.h

--- user.h
+++ user.h
@@ -23,6 +23,7 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+int wcupa(void);
 
 // ulib.c
 int stat(const char*, struct stat*);
@@ -37,4 +38,3 @@ void* memset(void*, int, uint);
 void* malloc(uint);
 void free(void*);
 int atoi(const char*);
-int wcupa(void);
